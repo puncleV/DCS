8a983a2a9d45858446d002a9540429cbf98bfd4a
Victor Kozlov <punkrus18@gmail.com>
2017-04-04 14:08:53 +0300

    IDE add std exit codes;

--- ipc.c
+++ ipc.c
@@ -2,21 +2,27 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 int main(int argc, char *const argv[])
 {
-	int opt, processCount;
-	while ((opt = getopt(argc, argv, "p:")) != -1) {
-		switch (opt) {
-		case 'p':
-			processCount = atoi(optarg);
-		break;
-		default:
-			fprintf(stderr, "Usage: %s [-p processCount]\n",
-			   argv[0]);
-			exit(-1);
-		}
-	}
+    int opt, processCount;
+//	pid_t *pids;
 
-	printf("Count: %d\n", processCount);
-	return 0;
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+        case 'p':
+            processCount = atoi(optarg);
+                if( processCount == 0 ) { exit(EXIT_FAILURE); }
+        break;
+        default:
+            fprintf(stderr, "Usage: %s [-p processCount]\n",
+               argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Count: %d\n", processCount);
+    exit(EXIT_SUCCESS);
 }
